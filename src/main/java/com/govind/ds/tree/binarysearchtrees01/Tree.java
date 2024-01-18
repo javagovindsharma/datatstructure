871d2c7849efe63797051f81d034b43f80806c4e
Govind Sharma <gsharma@confluence.com>
2024-01-18 13:31:15 +0530

    added tree

---
+++ src/main/java/com/govind/ds/tree/binarysearchtrees01/Tree.java
@@ -0,0 +1,23 @@
+package com.govind.ds.tree.binarysearchtrees01;
+
+public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+
+
+}
