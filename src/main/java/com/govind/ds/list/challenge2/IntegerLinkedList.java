5035f46322d3190b294360afe7affe38ae66516e
Govind Sharma <gsharma@confluence.com>
2024-01-18 12:54:50 +0530

    list items added

---
+++ src/main/java/com/govind/ds/list/challenge2/IntegerLinkedList.java
@@ -0,0 +1,52 @@
+package com.govind.ds.list.challenge2;
+
+public class IntegerLinkedList {
+
+    private IntegerNode head;
+    private int size;
+
+    public void addToFront(Integer value) {
+        IntegerNode node = new IntegerNode(value);
+        node.setNext(head);
+        head = node;
+        size++;
+    }
+
+    public IntegerNode removeFromFront() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        IntegerNode removedNode = head;
+        head = head.getNext();
+        size--;
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public void insertSorted(Integer value) {
+
+        // add your code here
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void printList() {
+        IntegerNode current = head;
+        System.out.print("HEAD -> ");
+        while (current != null) {
+            System.out.print(current);
+            System.out.print(" -> ");
+            current = current.getNext();
+        }
+        System.out.println("null");
+    }
+
+}
